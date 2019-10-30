6bdb5bc15ca6314bcb978ad5b56fde659599cf7c
Reasel <tannermjelde@gmail.com>
2019-10-30 14:31:52 -0700

    Update the script to remove un-needed things

--- ChestFarmer.py
+++ ChestFarmer.py
@@ -7,36 +7,17 @@ import math
 
 TotalAds = 0
 
-def touchRange(x1, x2, y1, y2):
+def touch(x1, x2, y1, y2):
 	startY = random.randrange(y1, y2)
 	startX = random.randrange(x1, x2)
 	endX = random.randrange(x1, x2)
 	endY = random.randrange(y1, y2)
 	device.drag((startX, startY),(endX, endY), 0.05,3)
 
-def touch(x, y):
-	#rint "Touching " + str(x) + ", " + str(y)
-	device.touch(x, y, 'DOWN_AND_UP')
-
-#def touchRange(x1, x2, y1, y2):
-#	touch(random.randrange(x1, x2), random.randrange(y1, y2))
-
-def clickButtonByCords(buttonName, x1, x2, y1, y2):
-	print "Click the " + buttonName + " button..."
-	touchRange(x1, x2, y1, y2)
-	time.sleep(2)
-
 def clickButton(button):
 	print "Click the " + button[2] + " button..."
-	touchRange(button[0][0], button[0][1], button[1][0], button[1][1])
+	touch(button[0][0], button[0][1], button[1][0], button[1][1])
 	time.sleep(2)
-
-def dragLeftRange(y1, y2):
-	startY = random.randrange(y1, y2)
-	startX = random.randrange(1250, 1320)
-	endX = random.randrange(1100, 1300)
-	endY = random.randrange(y1, y2)
-	device.drag((startX, startY),(endX, endY), 1,3)
 
 def manyClickRange(x1, x2, y1, y2, numClick):
 	baseX = random.randrange(x1, x2)
@@ -44,28 +25,6 @@ def manyClickRange(x1, x2, y1, y2, numClick):
 	for i in range(numClick):
 		touch(baseX + random.randrange(-5, 5), baseY + random.randrange(-5, 5))
 		time.sleep(200 / 1000)
-
-def wakeupPhone():
-	print "Unlocking Phone..."
-	print "   Waking up..."
-	device.wake()
-
-def unlockPhone():
-	print "   Swiping right..."
-	device.drag((170, 1700),(1200, 1700), 1,3)
-	time.sleep(1)
-	print "   Entering Password..."
-	touch(316, 1505)
-	time.sleep(500 / 1000)
-
-	touch(1111, 1530)
-	time.sleep(500 / 1000)
-
-	touch(1111, 1530)
-	time.sleep(500 / 1000)
-
-	touch(290, 1935)
-	time.sleep(500 / 1000)
 
 def betterSleep(n):
 	minuteNum = int(math.floor(n / 60))
@@ -103,14 +62,14 @@ def betterSleep(n):
 def clickChestRange(chestRange):
 	print "Clicking chest area..."
 	for x in range(chestRange[0][0], chestRange[0][1], 30):
-		#touch((x + random.randrange(-5, 5)), random.randrange(chestRange[1][0], chestRange[1][1]))
-		touchRange((x + random.randrange(-5, -1)), (x + random.randrange(1, 5)), chestRange[1][0], chestRange[1][1])
+		touch((x + random.randrange(-5, -1)), (x + random.randrange(1, 5)), chestRange[1][0], chestRange[1][1])
 
 def CheckWatchAd():
 	global TotalAds
 	print "Checking for ad..."
 	pixel = device.takeSnapshot().getRawPixel(811, 1283)
 	if pixel[1] == 74 and pixel[2] == 223 and pixel[3] == 255:
+		# Configure these manually
 		StartAd = ((534, 911),(1185, 1364), "start ad")
 		GetFlooz = ((645, 850),(1666, 1760), "get flooz")
 		print "Ad found starting!!!!"
@@ -125,35 +84,12 @@ def CheckWatchAd():
 
 
 # connection to the current device, and return a MonkeyDevice object
-device = MonkeyRunner.waitForConnection(10000, "a60e0c51")
+device = MonkeyRunner.waitForConnection(10000, "yourdeviceidhere")
 
+# Configure this manually
 ChestRange = ((480, 1392), (565, 575))
 
 while True:
 	clickChestRange(ChestRange)
 	CheckWatchAd()
 	time.sleep(2)
-
-
-
-
-
-#578 1519
-#875 1635
-
-
-# CTA package is com.godzilab.idlerpg
-# Width  is: 1440
-# Height is: 2560
-# Chests are 50 wide
-
-# Height 550-570
-# Width 480-1370
-
-#device.wake()
-#result = device.takeSnapshot()
-#
-#result.writeToFile('C:/Users/tmjelde/Documents/AndroidDev/screenshots/lockscreen.png','png')
-#device.drag((170, 1700),(1200, 1700), 1,3)
-#device.type('TESTING')
-print "end of script"
